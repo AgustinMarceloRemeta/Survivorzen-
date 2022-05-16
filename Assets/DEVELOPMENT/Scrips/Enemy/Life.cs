12a3132d62fda4a4793838842beac0f4d51214f3
AgustinMarceloRemeta <agustinmarceloremeta@gmail.com>
2022-05-16 02:49:10 -0300

    Maps

---
+++ Assets/DEVELOPMENT/Scrips/Enemy/Life.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Life : MonoBehaviour
+{
+    [SerializeField] float CountLife;
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
