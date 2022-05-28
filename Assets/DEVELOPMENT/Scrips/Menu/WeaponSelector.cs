58274670eb6a6b29639eb51920c203b26bb561a8
RoberP1 <roberpapeschi@gmail.com>
2022-05-28 01:58:00 -0300

    hud

---
+++ Assets/DEVELOPMENT/Scrips/Menu/WeaponSelector.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSelector : MonoBehaviour
+{
+    public int i;
+    public AimController aimController;
+    public Image Weapon;
+    public Sprite[] weapons;
+    public GameObject[] shopimage;
+    void Start()
+    {
+        i = PlayerPrefs.GetInt("weaponSelected", 0);
+        shopimage[i].SetActive(true);
+        aimController.ChangeGun(i);
+    }
+
+    public void nextWeapon()
+    {
+        shopimage[i].SetActive(false);
+        i++;
+        if (i >= 3) i = 0;
+        PlayerPrefs.SetInt("weaponSelected", i);
+        Weapon.sprite = weapons[i];
+        shopimage[i].SetActive(true);
+        aimController.ChangeGun(i);
+    }
+    public void preWeapong()
+    {
+        shopimage[i].SetActive(false);
+        i--;
+        if (i <= -1) i = 2;
+        PlayerPrefs.SetInt("weaponSelected", i);
+        Weapon.sprite = weapons[i];
+        shopimage[i].SetActive(true);
+        aimController.ChangeGun(i);
+    }
+}
